7a8de23831cd8060b82e1123e6dc73835f386418
Jonas Grau <jonas@subvision.dk>
2010-11-06 17:12:18 +0100

    that's more like me...

--- main.rb
+++ main.rb
@@ -17,16 +17,16 @@ gem 'ruby-debug', :group => :test
 gem 'infinity_test', :group => :test
 gem 'ruby-prof', :group => :test
 
-run "bundle install"
+# run "bundle install"
 
-generate "devise:install"
+# generate "devise:install"
 
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/jquery.rails.js"
-get "http://github.com/jgrau/rails3_template/raw/master/gitignore" ,".gitignore" 
-get "http://github.com/jgrau/rails3_template/raw/master/styles.css", "app/stylesheets/styles.css"
-get "http://github.com/jgrau/rails3_template/raw/master/print.css", "app/stylesheets/print.css"
-get "http://github.com/jgrau/rails3_template/raw/master/ie.css", "app/stylesheets/ie.css"
-get "http://github.com/jgrau/rails3_template/raw/master/application.html.haml", "app/views/layouts/application.html.haml"
+get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/jquery.rails.js"
+get "https://github.com/jgrau/rails3_template/raw/master/gitignore" ,".gitignore" 
+get "https://github.com/jgrau/rails3_template/raw/master/styles.css", "app/stylesheets/styles.css"
+get "https://github.com/jgrau/rails3_template/raw/master/print.css", "app/stylesheets/print.css"
+get "https://github.com/jgrau/rails3_template/raw/master/ie.css", "app/stylesheets/ie.css"
+get "https://github.com/jgrau/rails3_template/raw/master/application.html.haml", "app/views/layouts/application.html.haml"
 
 git :init
 git :add => '.'
